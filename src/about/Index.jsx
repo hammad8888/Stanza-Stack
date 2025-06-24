8b740cab792b734674c9c911a30fbe634a409880
Hammad Mustafa <hmustafa8888@gmail.com>
2025-06-24 08:53:43 -0700

    Path up

--- src/about/Index.jsx
+++ src/about/Index.jsx
@@ -3,7 +3,7 @@ import "animate.css";
 import Abouts from "../components/About";
 import Contact from "../components/ContactForm";
 import Footer from "../components/Footer";
-import Team from "../components/teamForm";
+// import Team from "../components/teamForm";
 import PS from "../components/AboutUs";
 const About = () => {
   return (
